4e43a4f15b35cae55b4835a91389d776b15ca858
Ruan Montelo <ruanmontelo@gmail.com>
2019-08-31 17:08:39 -0300

    Bot logging into discord and first commands.

--- Regalia.net/Program.cs
+++ Regalia.net/Program.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using Discord.Net;
+using System;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using Discord;
+using Discord.Commands;
 
 namespace Regalia.net
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            DiscordSocketClient _client = new DiscordSocketClient();
+
+            string token = "NTc4NzM2MTMwNjY5OTM2NjQw.XWrOAg._ap6Oq-FSo1n1gMu5mibq2SLJQ4";
+
+            await _client.LoginAsync(Discord.TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+
+            Console.ReadKey();
         }
     }
 }
