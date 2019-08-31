504c6db9b27925a18e8cef31dc8b46c24a9e30b3
Ruan Montelo <ruanmontelo@gmail.com>
2019-08-31 16:25:06 -0300

    First commit to Regalia.net

---
+++ Regalia.net/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Regalia.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
