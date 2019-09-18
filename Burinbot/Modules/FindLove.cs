82f59162bb555ed833618fe8c5d3fe8ed99742ef
Ruan Montelo <ruanmontelo@gmail.com>
2019-09-17 23:23:26 -0300

    Fixing the highestscoreanime command, new highestscoremanga command and findlove command.

---
+++ Burinbot/Modules/FindLove.cs
@@ -0,0 +1,17 @@
+﻿using Discord.Commands;
+using System;
+using System.Threading.Tasks;
+
+namespace Burinbot.Modules
+{
+    public class FindLove : ModuleBase<SocketCommandContext>
+    {
+        [Command("findlove")]
+        [Summary("Ah, I see you're a man of culture as well.")]
+        public async Task FindLoveAsync()
+        {
+            Random rnd = new Random();
+            await ReplyAsync(rnd.Next(1, 290000).ToString());
+        }
+    }
+}
--- Burinbot/Modules/HighestScoreManga.cs
+++ Burinbot/Modules/HighestScoreManga.cs
@@ -7,23 +7,23 @@ using System.Threading.Tasks;
 
 namespace Burinbot.Modules
 {
-    public class HighestScoreAnime : ModuleBase<SocketCommandContext>
+    public class HighestScoreManga : ModuleBase<SocketCommandContext>
     {
-        [Command("highestscoreanimes")]
+        [Command("highestscoremangas")]
         [Summary("Returns a list with 25 of the highest scored animes!")]
-        public async Task GetHighScoreAnimesAsync()
+        public async Task GetHighScoreMangasAsync()
         {
             try
             {
                 var builder = new EmbedBuilder()
                 {
-                    Title = "Highest rated animes!",
+                    Title = "Highest rated mangas!",
                     Color = Color.Green,
-                    Description = "These are the animes I found based on your request!"
+                    Description = "These are the mangas I found based on your request!"
                 };
 
-                var Client = new RestClient($"https://api.jikan.moe/v3/search/anime?order_by=score");
-                var Response = Client.Execute<AnimeSearch>(new RestRequest());
+                var Client = new RestClient($"https://api.jikan.moe/v3/search/manga?order_by=score");
+                var Response = Client.Execute<MangaSearch>(new RestRequest());
 
                 if (Response.StatusCode.Equals(System.Net.HttpStatusCode.BadRequest) || Response.StatusCode.Equals(System.Net.HttpStatusCode.InternalServerError))
                 {
@@ -33,24 +33,24 @@ namespace Burinbot.Modules
 
                 if (Response.Data.Results.Count == 0)
                 {
-                    await ReplyAsync("I didn't find any animes. That's weird. :thinking:");
+                    await ReplyAsync("I didn't find any mangas. That's weird. :thinking:");
                     return;
                 }
 
-                AnimeSearch animes = new AnimeSearch();
+                MangaSearch mangas = new MangaSearch();
 
-                foreach (Anime item in Response.Data.Results)
+                foreach (Manga item in Response.Data.Results)
                 {
-                    if (animes.Results.Count < 25)
-                        animes.Results.Add(item);
+                    if (mangas.Results.Count < 25)
+                        mangas.Results.Add(item);
                 }
 
-                foreach (Anime anime in animes.Results)
+                foreach (Manga manga in mangas.Results)
                 {
                     builder.AddField(x =>
                     {
-                        x.Name = $"{anime.Title ?? anime.Name}";
-                        x.Value = $"Link: {anime.URL}\nScore: {anime.Score}";
+                        x.Name = $"{manga.Title ?? manga.Name}";
+                        x.Value = $"Link: {manga.URL}\nScore: {manga.Score}";
                         x.IsInline = false;
                     });
                 }
